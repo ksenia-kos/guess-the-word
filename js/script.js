8c863b4628f62078a39d3a2b98c117d183c2d043
ksenia_k <kosykhksenia@gmail.com>
2021-04-25 00:44:16 -0400

    fetch words & remaining guesses

--- js/script.js
+++ js/script.js
@@ -15,20 +15,31 @@ const message = document.querySelector(".message");
 // hidden button that will appear prompting the player to play again
 const buttonPlayAgain = document.querySelector("button.play-again");
 
-const word = "magnolia";
+let word = "magnolia";
+let remainingGuesses = 8;
 // array that holds the letters the user guessed
 const guessedLetters = [];
+
+const getWord = async function () {
+    const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await request.text();
+    const wordArray = words.split("\n");
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    // pull random word from the wordArray & remove extra whitespace
+    word = wordArray[randomIndex].trim();
+    addSymbols(word);
+};
+
+getWord();
 
 // Display symbols as letter placeholders
 const addSymbols = function (word) {
     const symbols = [];
-    for (let letter of word) {
+    for (const letter of word) {
         symbols.push("●");
     }
     wordInProgress.innerText = symbols.join("");
-}
-
-addSymbols(word);
+};
 
 buttonGuess.addEventListener("click", function (e) {
     // prevents the default behavior of clicking a button, the form submitting, and then reloading the page
@@ -69,8 +80,8 @@ const makeGuess = function (guess) {
         message.innerText = "You already guessed that letter, silly. Try again.";
     } else {
         guessedLetters.push(guess);
-        console.log(guessedLetters);
         updateGuessedLetters();
+        countGuesses(guess);
         updateWordInProgress(guessedLetters);
     }
 };
@@ -78,8 +89,8 @@ const makeGuess = function (guess) {
 // show guessed letters
 const updateGuessedLetters = function () {
     guessedLettersElem.innerHTML = "";
-    for (let letter of guessedLetters) {
-        let li = document.createElement("li");
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
         li.innerText = letter;
         guessedLettersElem.append(li);
     }
@@ -99,6 +110,25 @@ const updateWordInProgress = function (guessedLetters) {
     }
     wordInProgress.innerText = updatedWord.join("");
     checkWin();
+};
+
+const countGuesses = function (guess) {
+    const wordUpper = word.toUpperCase();
+    if (wordUpper.includes(guess)) {
+        message.innerText = `Good guess! The word has the letter ${guess}.`;
+    } else {
+        message.innerText = `Sorry, the word has no ${guess}.`;
+        remainingGuesses -= 1;
+    }
+
+    if (remainingGuesses === 0) {
+        message.innerText = `Game over! The word was "${word}".`;
+        remainingSpan.innerText = "0 guesses";
+    } else if (remainingGuesses === 1) {
+        remainingSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingSpan.innerText = `${remainingGuesses} guesses`;
+    }
 };
 
 // check if player won
